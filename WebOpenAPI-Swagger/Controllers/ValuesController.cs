465c5160efdaca4c56bad71b7de89ccdb23677e0
Luis Q Lu <luis.q.lu@pwc.com>
2020-03-24 16:44:56 +0800

    Add project files.

---
+++ WebOpenAPI-Swagger/Controllers/ValuesController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebOpenAPI_Swagger.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        /// <summary>
+        /// get api values
+        /// </summary>
+        /// <returns>return values</returns>
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        /// <summary>
+        /// get a value with specific id
+        /// </summary>
+        /// <param name="id">id</param>
+        /// <returns>return a value</returns>
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value 2";
+        }
+
+        /// <summary>
+        /// add a new value
+        /// </summary>
+        /// <param name="value">value</param>
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        /// <summary>
+        /// update a value
+        /// </summary>
+        /// <param name="id">value id</param>
+        /// <param name="value">value</param>
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
